d4cb451b0f12148da703ebb3bb4bfe560ae6f935
Jason <therawan.jason@gmail.com>
2025-09-16 00:00:22 +0700

    Add UI

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+
+let humanscore = document.querySelector("#humanscore");
+let computerscore = document.querySelector("#computerscore");
+const humanchoice = document.querySelector(".humanchoice");
+const computerchoice = document.querySelector(".computerchoice");
+const announcement = document.querySelector("#winner");
+
+const btns = document.querySelectorAll("button");
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        humanChoice = btn.id;
+        humanchoice.src = `assets/${btn.id}.jpeg`;
+        let computerChoice = getComputerChoice();
+        computerchoice.src = `assets/${computerChoice}.jpeg`;
+        playRound(humanChoice, computerChoice);
+    })
+})
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-    if (computerChoice == 1) return "Rock";
-    else if (computerChoice == 2) return "Paper";
-    else return "Scissors";
-}
-
-function getHumanChoice() {
-    return prompt("ROCK | PAPER | SCISSORS");
+    if (computerChoice == 1) return "rock";
+    else if (computerChoice == 2) return "paper";
+    else return "scissors";
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -17,25 +31,26 @@ function playRound(humanChoice, computerChoice) {
        (humanChoice.toLowerCase() == 'paper' && computerChoice.toLowerCase() == 'rock') ||
        (humanChoice.toLowerCase() == 'scissors' && computerChoice.toLowerCase() == 'paper')){
         humanScore++;
-        alert(`Human won! ${humanChoice} beats ${computerChoice}!`);
+        humanscore.textContent = `Your Score: ${humanScore}`;
+        announcement.textContent = `Human won! ${humanChoice} beats ${computerChoice}!`;
+        announcement.style.color = 'green';
     }
-    else if(humanChoice.toLowerCase() == computerChoice.toLowerCase())
-        alert("Draw!");
+    else if(humanChoice.toLowerCase() == computerChoice.toLowerCase()){
+        announcement.textContent = "Draw!";
+        announcement.style.color = 'black';
+    }
     else {
         computerScore++;
-        alert(`Computer won! ${computerChoice} beats ${humanChoice}!`);
+        computerscore.textContent = `Computer's Score: ${computerScore}`;
+        announcement.textContent = `Computer won! ${computerChoice} beats ${humanChoice}!`;
+        announcement.style.color = 'red';
     }
+    if(humanScore == 5) setTimeout(() => {
+        alert("HUMAN WON!!!");
+        location.reload();
+    }, 50);
+    if(computerScore == 5) setTimeout(() => {
+        alert("COMPUTER WON!!!");
+        location.reload();
+    }, 50);
 }
-
-function playGame() {
-    for(let round = 1; round <= 5; round++) {
-        let computerChoice = getComputerChoice();
-        let humanChoice = getHumanChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    if(humanScore > computerScore) alert("HUMAN WON!!!");
-    else if(humanScore < computerScore) alert("COMPUTER WON!!!");
-    else alert("NOBODY WON!!!");
-}
-
-playGame();
